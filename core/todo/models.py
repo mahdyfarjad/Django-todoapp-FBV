9505d2978dbe772cd8cd7583d72306fefaa9da50
mahdyfarjad <mahdyfarjad@yahoo.com>
2023-12-03 14:52:23 +0330

    remove description

--- core/todo/models.py
+++ core/todo/models.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import User
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=250)
-    description = models.TextField(null=True, blank=True)
     complete = models.BooleanField(default=False)
     created_date = models.DateTimeField(auto_now_add=True)
 
